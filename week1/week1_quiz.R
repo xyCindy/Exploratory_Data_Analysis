bcf530b1a0aaf1bf73b42fb432878be1b1109b39
Cindy <xinyuechen213@gmail.com>
2015-10-08 19:22:40 -0400

    week1 quiz completed

---
+++ week1/week1_quiz.R
@@ -0,0 +1,42 @@
+#Question 1
+#Which of the following is a principle of analytic graphics?
+#---Show causality, mechanism, explanation
+
+#Question 2
+#What is the role of exploratory graphs in data analysis?
+#---The goal is for personal understanding.
+
+#Question 3
+#Which of the following is true about the base plotting system?
+#---Plots are created and annotated with separate functions
+
+#Question 4
+#Which of the following is an example of a valid graphics device in R?
+#---A PDF file
+
+#Question 5
+#Which of the following is an example of a vector graphics device in R?
+#---Postscript
+
+#Question 6
+#Bitmapped file formats can be most useful for
+#---Scatterplots with many many points
+
+#Question 7
+#Which of the following functions is typically used to add elements to a plot 
+#in the base graphics system?
+#---lines()
+
+#Question 8
+#Which function opens the screen graphics device on Windows?
+#---windows()
+
+#Question 9
+#What does the 'pch' option to par() control?
+#---the plotting symbol/character in the base graphics system
+
+#Question 10
+#If I want to save a plot to a PDF file, which of the following is a correct 
+#way of doing that?
+#---Construct the plot on the screen device and then copy it to a PDF file with
+#---dev.copy2pdf()
